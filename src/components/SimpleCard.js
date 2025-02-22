d7591d485db44d3ccb96401edcd9d1f376773d06
Gayashan <36268158+GayashanWick@users.noreply.github.com>
2025-02-22 20:11:52 +0530

    innitial commit

---
+++ src/components/SimpleCard.js
@@ -0,0 +1,20 @@
+import './SimpleCard.css';
+
+
+
+function SimpleCard(){
+   // return <div> my simple card</div>
+
+   return(
+    <div className= "card-container">
+        <h1> Introduction</h1>
+        <p>
+            Lorem Ipsum, or lipsum as it is sometimes known,
+        </p>
+        <button>View More</button>
+    </div>
+   )
+
+}
+
+export default SimpleCard;
